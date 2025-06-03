a946c7ceff3481307588dcce329518b467bb1016
younss95 <younesazirgui@gmail.com>
2025-06-03 11:08:06 +0200

    add psycopg2 2

--- src/studelydesk/db.py
+++ src/studelydesk/db.py
@@ -1,30 +1,15 @@
-import os
-from urllib.parse import urlparse
 import psycopg2
 import sqlite3
+import os
 
 def get_db_connection():
     database_url = os.getenv('ARCHILOG_DATABASE_URL')
+
     if database_url.startswith("sqlite://"):
-        # Extraire chemin fichier SQLite
         path = database_url.replace("sqlite:///", "")
         conn = sqlite3.connect(path)
         return conn
     else:
-        # PostgreSQL
-        result = urlparse(database_url)
-        username = result.username
-        password = result.password
-        database = result.path[1:]
-        hostname = result.hostname
-        port = result.port
-
-        conn = psycopg2.connect(
-            dbname=database,
-            user=username,
-            password=password,
-            host=hostname,
-            port=port,
-            sslmode='require'
-        )
+        # Connexion PostgreSQL en passant l’URL complète (y compris les paramètres)
+        conn = psycopg2.connect(database_url)
         return conn
